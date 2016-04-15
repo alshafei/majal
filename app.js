25d544da121a38fb566666081e542d26ba4b6f07
Alvaro Cabrera <pateketrueke@gmail.com>
2016-04-15 11:09:10 -0500

    Fixed static routes

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ var express = require('express');
 
 var app = express();
 
-app.use('/vendor', express.static('bower_components'));
+app.use('/assets', express.static('build/public/assets'));
+app.use('/images', express.static('build/public/images'));
+app.use('/vendor', express.static('build/public/vendor'));
+app.use('/index.html', express.static('build/public/index.html'));
 
 app.get('/*', function (req, res) {
   res.send($.Majal.render(req.url));
